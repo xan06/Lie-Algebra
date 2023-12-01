351dbfff432cf3d209f43645cb68a0ba782edcf2
xan06 <xianhao2@illinois.edu>
2023-12-01 10:14:50 -0600

    Add check_jacobi

--- Lie test.py
+++ Lie test.py
@@ -15,6 +15,20 @@ def SC(basis, bracket_dict):
                 C[i][j][k]=expr.coeff(basis[k])
     return C
 
+# Check if structure constants satisfy jacobi identity.
+def check_jacobi_identity(basis, bracket_dict):
+    n = len(basis)
+    C = SC(basis, bracket_dict)
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                for m in range(n):  # Added innermost loop for 'm'
+                    sum_jacobi = 0
+                    for l in range(n):  # Added loop for 'l'
+                        sum_jacobi += C[i][j][l] * C[l][k][m] + C[j][k][l] * C[l][i][m] + C[k][i][l] * C[l][j][m]
+                    if sum_jacobi != 0:
+                        return False
+    return True
 # Consider Ad: g \to End(g)
 #         x \mapsto Ad_x:= [x , \cdot ]
 #     We have Ad(e_k)=k-th column of Ad, and note that Ad(x) is a n^2 vector which can be converted to a matrix T_x
@@ -55,22 +69,19 @@ def Ad(basis, bracket_dict):
         matrices[i]= Matrix(C[i])
     return Matrix(BlockMatrix(matrices)).nullspace()
 
+
+
 # test case
-e1,e2, e3= symbols('e1 e2 e3')
+e1,e2 = symbols('e1 e2')
 zero = symbols('0')
-basis = [e1, e2, e3]
+basis = [e1, e2]
 n= len(basis)
 e = [Matrix([int(i == j) for j in range(n)]) for i in range(n)]
 dictionary = {
     (e1,e1): zero,
-    (e1,e2): zero,
-    (e1,e3): e1,
+    (e1,e2): e2,
     (e2,e1): -1*(e2),
     (e2,e2): zero,
-    (e2,e3): e2,
-    (e3,e1):-1*e1,
-    (e3,e2):-1*e2,
-    (e3,e3):zero
    
 }
 C = SC(basis, dictionary)
